8f3fbc272060ea9aa8b44216196b99614953fe89
Tianrui Guo <tianruig@gmail.com>
2015-02-02 22:59:33 -0800

    Initial classes ListView setup
    Currently using dummy data to populate list

--- app/src/main/java/com/tianruiguo/checkr/MainActivity.java
+++ app/src/main/java/com/tianruiguo/checkr/MainActivity.java
@@ -1,15 +1,15 @@
 package com.tianruiguo.checkr;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -60,6 +60,21 @@ public class MainActivity extends ActionBarActivity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+            ArrayAdapter<String> classesAdapter =
+                    new ArrayAdapter<String>(
+                            getActivity(),
+                            R.layout.list_item_class,
+                            R.id.list_item_class_textview,
+                            new String[]{
+                                    "Math - 95%",
+                                    "Science - 93%",
+                                    "History - 90%"}
+                    );
+
+            ListView listViewClasses = (ListView) rootView.findViewById(R.id.listview_classes);
+            listViewClasses.setAdapter(classesAdapter);
+
             return rootView;
         }
     }
